56ebd62fc8f1b65b62fc2fc0498af92d93e7bb6d
Fakhar <fakharamirali@gmail.com>
2023-09-21 19:41:47 +0430

    Translated

--- payment/status.py
+++ payment/status.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class StatusChoices(models.IntegerChoices):
-    REFUND_FAILED_BY_LACK_OF_FUNDS = -3, _("Refund failed by lake of funds")
+    REFUND_FAILED_BY_LACK_OF_FUNDS = -3, _("Refund failed by lack of funds")
     REFUND_FAILED = -2, _("Refund Failed")
     REFUNDED = -1, _("Refunded")
     SUCCESSFUL = 0, _("Successful")
