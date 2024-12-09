e46698117096fdaf1d7728c14377ec8461b44511
Fakhar <fakharamirali@gmail.com>
2024-12-09 20:44:01 +0330

    Repair Packaging system

--- payment/__init__.py
+++ payment/__init__.py
@@ -1,10 +1,2 @@
-from django.utils.module_loading import autodiscover_modules
-
-from .registry import registry
-
 __version__ = '0.0.5b0'
 VERSION = (0, 0, 5, 'beta', 0)
-
-
-def autodiscover():
-    autodiscover_modules('payment_backends', register_to=registry)
